78c278c022db45a531d586223349255d1dd28cbe
Wayne <s0921233527@gmail.com>
2026-04-24 10:45:27 +0800

    feat:新增Tailwind CSS, 新增AppFooter

--- shizuku-vue/src/App.vue
+++ shizuku-vue/src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import AppFooter from './components/AppFooter.vue'
 </script>
 
-<template></template>
+<template>
+  <main class="flex-grow">
+    <h1 class="text-3xl font-bold p-10">歡迎來到 shizuku</h1>
+  </main>
+
+  <AppFooter />
+</template>
 
 <style scoped></style>
--- shizuku-vue/src/main.js
+++ shizuku-vue/src/main.js
@@ -3,12 +3,31 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import router from './router'
-
+import 'primeicons/primeicons.css'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import InputGroup from 'primevue/inputgroup'
+import Divider from 'primevue/divider'
+import Aura from '@primevue/themes/aura'
 const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark',
+    },
+  },
+})
 
 app.use(createPinia())
 app.use(router)
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.component('InputGroup', InputGroup)
+app.component('Divider', Divider)
 
 app.mount('#app')
