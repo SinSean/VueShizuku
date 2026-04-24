9d5315ee485a0327f1e2291ad39087ed14d11765
Wayne <s0921233527@gmail.com>
2026-04-24 09:58:05 +0800

    feat:移除不必要的範本

--- shizuku-vue/src/App.vue
+++ shizuku-vue/src/App.vue
@@ -2,17 +2,6 @@
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 
-<template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
+<template></template>
 
 <style scoped></style>
