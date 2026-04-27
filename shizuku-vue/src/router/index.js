ee1c90ba158d4dc4c2292d603883342f1b694cb0
Sean <sealll40@gmail.com>
2026-04-27 13:15:45 +0800

    fix: 修正路由設定問題 - index.js刪除About的路由

--- shizuku-vue/src/router/index.js
+++ shizuku-vue/src/router/index.js
@@ -9,14 +9,9 @@ const router = createRouter({
       component: Basic,
       children: [
         {
-          path: '', // 當路徑為 '/' 時，載入首頁
+          path: '',
           name: 'home',
-          component: () => import('../views/HomeView.vue'), // 假設您有這個檔案
-        },
-        {
-          path: 'about', // 當路徑為 '/about' 時，載入 About 頁面並顯示在 Basic 的 <RouterView /> 內
-          name: 'about',
-          component: () => import('../views/AboutView.vue'),
+          component: () => import('../views/HomeView.vue'),
         },
       ],
     },
