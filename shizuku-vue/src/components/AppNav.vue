472ebef083d40f03f90543d2dac8fc4ade8b3f00
Wayne <s0921233527@gmail.com>
2026-04-27 16:30:34 +0800

    Feat:修改CartMenu背景顏色

--- shizuku-vue/src/components/AppNav.vue
+++ shizuku-vue/src/components/AppNav.vue
@@ -1,9 +1,10 @@
+<script setup>
+import AppCartMenu from './AppCartMenu.vue'
+</script>
+
 <template>
   <nav class="fixed top-0 left-0 w-full z-50 bg-gray-100 py-4">
     <div class="flex justify-center items-center px-4 py-6">
-      <!-- <div class="absolute left-4 lg:left-8">
-        <span class="text-2xl font-bold italic tracking-tighter cursor-pointer text-black">Shizuku</span>
-      </div> -->
       <div class="absolute left-4 lg:left-8">
         <span class="text-2xl font-bold italic tracking-tighter cursor-pointer text-black">
           <img src="../assets/img/logo.png" alt="Shizuku" class="inline-block" />
@@ -30,17 +31,7 @@
           </svg>
         </button>
 
-        <button class="flex items-center gap-1 hover:text-gray-400 transition-colors">
-          <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            ></path>
-          </svg>
-          <span class="text-xs font-bold">0</span>
-        </button>
+        <AppCartMenu />
 
         <button
           class="border border-gray-300 px-3 py-1 rounded-full text-xs hover:bg-gray-50 transition-colors"
