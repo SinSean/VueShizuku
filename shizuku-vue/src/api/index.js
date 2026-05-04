a1fdc9bff67291018c046800415928dd06508a03
Sean <sealll40@gmail.com>
2026-05-04 15:48:07 +0800

    fix: 不放延遲時間登入

--- shizuku-vue/src/api/index.js
+++ shizuku-vue/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const request = axios.create({
     baseURL: 'https://localhost:7197/api',
-    timeout: 5000
+    //timeout: 5000   //不放延遲時間
 });
 
 // 你可以在這裡統一處理錯誤攔截 (Interceptors)
